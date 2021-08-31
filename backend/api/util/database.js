3bd553de1f25fa373386fa0f24eb718059ae7765
Michael Kang <mrmichaelkang@gmail.com>
2021-08-30 21:28:40 -0700

    set up database connection

--- backend/api/util/database.js
+++ backend/api/util/database.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+// const sequelize = new Sequelize(process.env.DATABASE_URL);
 let db = {}
-// const sequelize = new Sequelize(`${process.env.DB}`, `${process.env.DB_USER}`, `${process.env.DB_PASS}`, {
-//   dialect: 'postgres',
-//   host: `${process.env.DB_HOST}`
-// });
+const sequelize = new Sequelize(`${process.env.DB}`, `${process.env.DB_USER}`, `${process.env.DB_PASS}`, {
+  dialect: 'postgres',
+  host: `${process.env.DB_HOST}`
+});
 
 module.exports = openDB = async () => {
   try {
