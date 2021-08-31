e3cb837cc53fcd65660cf36e5fc6ab0e3f7b392c
Michael Kang <mrmichaelkang@gmail.com>
2021-08-30 21:12:09 -0700

    set up database connection

--- backend/api/util/database.js
+++ backend/api/util/database.js
@@ -1,12 +1,10 @@
-const connection = 'postgres://postgres:mk4211310@localhost:5432/mailroom';
-// const db = pgp(connection);
-let db = {}
-
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(`${process.env.DB}`, `${process.env.DB_USER}`, `${process.env.DB_PASS}`, {
-  dialect: 'postgres',
-  host: `${process.env.DB_HOST}`
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+let db = {}
+// const sequelize = new Sequelize(`${process.env.DB}`, `${process.env.DB_USER}`, `${process.env.DB_PASS}`, {
+//   dialect: 'postgres',
+//   host: `${process.env.DB_HOST}`
+// });
 
 module.exports = openDB = async () => {
   try {
